00384ed048b31c7a5cd975592911584673b6ca8f
Nathaniel Frimpong-Santeng <nath.fripm.san@gmail.com>
2025-03-20 02:24:55 +0000

    Create PHYSICS ENGINE

---
+++ ApplicationEngine/src/Physics/LNL_Physics.py
@@ -0,0 +1,30 @@
+from ApplicationEngine.include.Maths.Maths import *
+from ApplicationEngine.src.Object.Object import *
+
+
+
+
+
+def QuadColision(quad1, quad2):
+    ...
+
+
+
+def CuboidCollision(cuboid1, cuboid2):
+    ...
+
+
+
+def CuboidOnSphere(cuboid, sphere):
+    ...
+
+
+def PoinOnCuboid(point : Vec3, cuboid):
+    ...
+
+
+class Ray:
+    ...
+
+class ForceGenerator:
+    ...
