70efbeb7a813c7585b4d5687ac9a006ebfb9872c
Nathaniel Frimpong-Santeng <nath.fripm.san@gmail.com>
2025-03-31 14:29:16 +0100

    demo concept 1

--- Game/src/Level/TutorialLevel.py
+++ Game/src/Level/TutorialLevel.py
@@ -26,9 +26,9 @@ class TutorialLevel(LNLEngine.Level):
         tex1 = Texture("Game/Assets/Sprites/environ/rocksfull.png", True)
         
         chunk0 = TileChunk(Vec2(-1,8),Vec2(50, 2), tex0, tex1)
-        chunk1 = TileChunk(Vec2(-1,3),Vec2(2, 5), tex0, tex1)
-        chunk2 = TileChunk(Vec2(10,3),Vec2(2, 5), tex0, tex1)
-        chunk3 = TileChunk(Vec2(15,5),Vec2(2, 5), tex0, tex1)
+        chunk1 = TileChunk(Vec2(-1,3),Vec2(2, 6), tex0, tex1)
+        chunk2 = TileChunk(Vec2(10,3),Vec2(2, 6), tex0, tex1)
+        chunk3 = TileChunk(Vec2(15,5),Vec2(2, 6), tex0, tex1)
         self.tiles.append(chunk0)
         self.tiles.append(chunk1)
         self.tiles.append(chunk2)
