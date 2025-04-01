2e9c19baf6ccae8566b33453d09c7933262b4aba
LAPTOP-1FQP84I8\callu <cjvf2211@gmail.com>
2025-04-02 00:38:22 +0100

    Slight box changes for colliders

--- Game/src/GameComponents/Environement/Box.py
+++ Game/src/GameComponents/Environement/Box.py
@@ -8,8 +8,30 @@ class Box(EnvironmentObject2D):
         self.dimensions = dimensions * WorldGrid.GRID_SIZE  
         self.sprite = LNLEngine.Sprite(Box.boxTexture, self.body.position - self.dimensions/2, self.dimensions.x, self.dimensions.y)
         
+        self.colliders : list[LNLEngine.Collider2D] = [] 
+        col1 = self._InitCollider(self.dimensions) 
+
+        self.body.setCollider(col1) 
+        self.colliders.append(col1)
+
 
  
 
 
-    
+    def _InitCollider(self, size : Vec2) -> LNLEngine.Collider2D:
+        """seperate colider init for modification in inherited classes
+
+        Args:
+            size (Vec2): width and height [use teh first index as radius if dealing with a sphere]
+
+        Returns:
+            LNLEngine.Collider2D: collision shape
+        """
+
+
+        c1 = LNLEngine.Box2D()
+        # c1 = LNLEngine.AABB()
+        c1.setRigidBody(self.body)
+        c1.setSize(size)
+
+        return c1
